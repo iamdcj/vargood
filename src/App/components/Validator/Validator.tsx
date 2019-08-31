9d7766780ce3e95701c455cd8e4a48843f9aeb9e
David Christian Jones <david@dcj.codes>
2019-08-30 22:18:36 -0400

    Regex switcher

--- src/App/components/Validator/Validator.tsx
+++ src/App/components/Validator/Validator.tsx
@@ -1,41 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/index.css';
+import { es5, es5Strict, es6, es6Strict } from './_constants';
 
 interface Props {}
 
 const Validator: React.FunctionComponent<Props> = () => {
   const validate = (identifier: string) => {
-    console.log(identifier);
+    setValue(identifier);
+
+    switch (radioSelection) {
+      case 'es6': {
+        const valid = es6.test(identifier);
+        validator(valid);
+        break;
+      }
+      default: {
+        const valid = es5.test(identifier);
+        validator(valid);
+        break;
+      }
+    }
   };
 
+  const [isStrict, handleStrict] = useState(false);
+  const [radioSelection, handleRadio] = useState('es5');
+  const [value, setValue] = useState('');
+  const [valid, validator] = useState(false);
+
   return (
-    <section className='validator'>
-      {/* <h1 className='validator__title'>Validate Identifier</h1> */}
-      <div className='control control--fw'>
-        <label htmlFor='validator' className='validator__subtitle'>
-          Please enter your desired variable name
-        </label>
-        <div>
-          <input
-            onChange={({ target }) => validate(target.value)}
-            className='control__input'
-            type='text'
-            name='validator'
-            id='validator'
-          />
+    <section
+      className={`validator ${
+        value ? (valid ? 'is--valid' : 'is--invalid') : ''
+      }`}
+    >
+      <div className='validator__inner'>
+        {/* <h1 className='validator__title'>Validate Identifier</h1> */}
+        <div className='control control--fw'>
+          <label htmlFor='validator' className='validator__subtitle'>
+            Please enter your desired variable name
+          </label>
+          <div>
+            <input
+              onChange={({ target }) => validate(target.value)}
+              className='control__input'
+              type='text'
+              name='validator'
+              id='validator'
+              value={value}
+            />
+          </div>
         </div>
-      </div>
-      <div className='control  control--flex'>
-        <div>
-          <input
-            onChange={({ target }) => validate(target.value)}
-            className='control__input'
-            type='checkbox'
-            name='use-strict'
-            id='use-strict'
-          />
+        <div className='control  control--flex'>
+          <div>
+            <input
+              onChange={({ target }) => handleRadio(target.value)}
+              className='control__input'
+              type='radio'
+              name='version'
+              id='es5'
+              value='es5'
+              checked={radioSelection === 'es5' ? true : false}
+            />
+          </div>
+          <label htmlFor='use-strict'></label>
         </div>
-        <label htmlFor='use-strict'>Strict mode?</label>
+        <div className='control  control--flex'>
+          <div>
+            <input
+              onChange={({ target }) => handleRadio(target.value)}
+              className='control__input'
+              type='radio'
+              name='version'
+              id='es6'
+              value='es6'
+              checked={radioSelection === 'es6' ? true : false}
+            />
+          </div>
+          <label htmlFor='use-strict'></label>
+        </div>
+        <div className='control  control--flex'>
+          <div>
+            <input
+              onChange={({ target }) => handleStrict(target.checked)}
+              className='control__input'
+              type='checkbox'
+              name='use-strict'
+              id='use-strict'
+            />
+          </div>
+          <label htmlFor='use-strict'>Strict mode?</label>
+        </div>
       </div>
     </section>
   );
