4218ca65cf401aed2ba25a240a9689ca61765b95
David Christian Jones <david@dcj.codes>
2019-09-03 08:08:20 -0400

    fix es6 strict

--- src/App/_constants.ts
+++ src/App/_constants.ts
@@ -7,7 +7,7 @@ const es5Strict = {
 };
 
 const es6 = {
-  keyword: /^(?:do|if|in|for|let|new|try|var|case|else|enum|eval|false|null|this|true|void|with|await|break|catch|class|const|super|throw|while|yield|delete|export|import|public|return|static|switch|typeof|default|extends|finally|package|private|continue|debugger|function|arguments|interface|protected|implements|instanceof)$/
+  keyword: /^(?:do|if|in|for|let|new|try|var|case|else|enum|eval|false|null|this|true|void|with|await|break|catch|class|const|super|throw|while|yield|delete|export|import|return|static|switch|typeof|default|extends|finally|package|continue|debugger|function|arguments|instanceof)$/
 };
 
 const es6Strict = {
