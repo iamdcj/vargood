830bf937be1ceaba4df0ee8bfbc28b880819c131
David Christian Jones <david@dcj.codes>
2019-09-17 18:59:31 -0400

    Update ALL supported version keywords as per spec

--- src/App/_constants.ts
+++ src/App/_constants.ts
@@ -12,18 +12,18 @@ interface Keywords {
 const versions = ['es6', 'es5', 'es3'];
 
 const keywords: Keywords = {
-  es3: /^(?:int|byte|char|goto|long|final|float|short|double|native|throws|boolean|abstract|volatile|transient|synchronized)$/,
+  es3: /^(?:break|continue|delete|else|abstract|boolean|byte|case|catch|char|class|const|debugger|default|for|function|if|in|do|double|enum|export|extends|final|finally|float|goto|implements|new|return|this|typeof|import|instanceof|int|interface|long|native|package|private|protected|public|var|void|while|with|short|static|super|switch|synchronized|throw|throws|transient|try|volatile|)$/,
   es5: /^(?:do|if|in|for|new|try|var|case|else|enum|eval|false|null|this|true|void|with|break|catch|class|const|super|throw|while|delete|export|import|return|switch|typeof|default|extends|finally|continue|debugger|function|arguments|instanceof)$/,
-  es5Strict: /^(?:do|if|in|for|new|try|var|case|else|enum|eval|false|null|this|true|void|with|break|catch|class|const|super|throw|while|delete|export|import|return|switch|typeof|default|extends|finally|continue|debugger|function|arguments|instanceof|implements|let|private|public|yield|interface|package|protected|static)$/,
+  es5Strict: /^(?:break|case|catch|continue|debugger|default|in|extends|import|do|else|finally|for|function|if|try|super|instanceof|new|return|switch|this|throw|class|const|typeof|var|void|while|with|delete|enum|export|implements|interface|yield|let|private|public|package|protected|static)$/,
   es6: /^(?:break|case|catch|class|const|continue|debugger|if|try|do|else|export|extends|finally|for|function|throw|await|in|instanceof|new|return|super|switch|this|delete|typeof|var|void|while|with|yield|default|import)$/,
   es6Strict: /^(?:break|case|catch|class|const|continue|debugger|if|try|do|else|export|extends|finally|for|function|throw|await|in|instanceof|new|return|super|switch|this|delete|typeof|var|void|while|with|yield|default|import|let|implements|package|protected|static|interface|private|public)$/,
   warning: /^(?:nan|NaN|undefined|Infinity)$/i
 };
 
 const messages = {
-  keyword: '🚫 Desired identifier conflicts with a keyword.',
+  keyword: 'Desired identifier conflicts with a keyword.',
   format:
-    '🚫 Desired identifier cannot contain spaces. Identifier must start with A-Z, $, or _',
+    'Desired identifier cannot contain spaces. Identifier must start with A-Z, $, or _',
   sketchy: "⚠️ You can, but you shouldn't",
   valid: 'All Good 👍'
 };
